2adc9cb19bac0c1a193b3b1ea3134ac856d2a894
Alvin.Avocados <alvinpang321@gmail.com>
2021-09-17 17:13:57 +0800

    Add printDeadlinesUsingStream

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -52,4 +52,11 @@ public class Main {
             }
         }
     }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing deadlines using stream");
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline) // filtering using lambda
+                .forEach(System.out::println);
+    }
 }
